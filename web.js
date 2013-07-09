6680c0c2c5ea45ed1c76590a6a44aa82000e3bd9
Cristi Bolea <cristianbolea@gmail.com>
2013-07-09 11:33:26 +0000

    Added toString

--- web.js
+++ web.js
@@ -2,8 +2,13 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+
+
+app.get('/', function(request, response)
+
+{
+  var sent = fs.readFileSync('/bitstarter/index.html').toString('utf-8');
+  response.send(sent);
 });
 
 var port = process.env.PORT || 5000;
